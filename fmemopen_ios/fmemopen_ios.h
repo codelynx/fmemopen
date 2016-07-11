ea8fb022b61a7441a56dcc4b93dc1f69501eea1c
Kaz Yoshikawa <kaz@digitallynx.com>
2016-07-12 01:22:36 +0900

    provide ios, mac target

---
+++ fmemopen_ios/fmemopen_ios.h
@@ -0,0 +1,19 @@
+//
+//  fmemopen_ios.h
+//  fmemopen_ios
+//
+//  Created by Kaz Yoshikawa on 7/12/16.
+//  Copyright © 2016 Electricwoods LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for fmemopen_ios.
+FOUNDATION_EXPORT double fmemopen_iosVersionNumber;
+
+//! Project version string for fmemopen_ios.
+FOUNDATION_EXPORT const unsigned char fmemopen_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <fmemopen_ios/PublicHeader.h>
+
+
---
+++ fmemopen_mac/fmemopen_mac.h
@@ -0,0 +1,19 @@
+//
+//  fmemopen_mac.h
+//  fmemopen_mac
+//
+//  Created by Kaz Yoshikawa on 7/12/16.
+//  Copyright © 2016 Electricwoods LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for fmemopen_mac.
+FOUNDATION_EXPORT double fmemopen_macVersionNumber;
+
+//! Project version string for fmemopen_mac.
+FOUNDATION_EXPORT const unsigned char fmemopen_macVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <fmemopen_mac/PublicHeader.h>
+
+
